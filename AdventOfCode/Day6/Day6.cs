5eba5663ced0113651b3e16747cf81f794fd8689
Björn Wallentin <bjorn.wallentin@voyado.com>
2022-12-06 09:31:40 +0100

    Day6

---
+++ AdventOfCode/Day6/Day6.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode.Day6
+{
+    internal class Day6
+    {
+        internal void Work()
+        {
+            string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Day6\input.txt");
+            var input = File.ReadAllText(filePath);
+            
+            var first = Solve(input, 4);
+            var second = Solve(input, 14);
+        }
+
+        private int Solve(string input, int distinctChars)
+        {
+            for (int i = 0; i < (input.Length - (distinctChars-1)); i++)
+            {
+                var text = input.Substring(i, distinctChars);
+
+                if (text.Distinct().Count() == distinctChars)
+                {
+                    return i + distinctChars;
+                }
+            }
+            return 0;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,9 +3,11 @@ using AdventOfCode.Day2;
 using AdventOfCode.Day3;
 using AdventOfCode.Day4;
 using AdventOfCode.Day5;
+using AdventOfCode.Day6;
 
 //new Day1().Work();
 //new Day2().Work();
 //new Day3().Work();
 //new Day4().Work();
-new Day5().Work();
+//new Day5().Work();
+new Day6().Work();
